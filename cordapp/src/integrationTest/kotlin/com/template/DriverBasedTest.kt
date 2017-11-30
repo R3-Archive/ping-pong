e2247da0099add100489a7ecbe4ee65709a0f675
joeldudleyr3 <joel.dudley@r3.com>
2017-11-30 16:37:35 +0000

    Initial commit.

---
+++ cordapp/src/integrationTest/kotlin/com/template/DriverBasedTest.kt
@@ -0,0 +1,39 @@
+package com.template
+
+import net.corda.core.utilities.getOrThrow
+import net.corda.node.services.transactions.SimpleNotaryService
+import net.corda.nodeapi.User
+import net.corda.nodeapi.internal.ServiceInfo
+import net.corda.testing.DUMMY_BANK_A
+import net.corda.testing.DUMMY_BANK_B
+import net.corda.testing.DUMMY_BANK_C
+import net.corda.testing.DUMMY_NOTARY
+import net.corda.testing.driver.driver
+import org.junit.Test
+import kotlin.test.assertFailsWith
+
+class DriverBasedTest {
+    @Test
+    fun `run driver test`() {
+        val user = User("user1", "test", permissions = setOf("StartFlow.com.template.Ping"))
+
+        driver(startNodesInProcess = true) {
+            val (_, nodeAHandle, _) = listOf(
+                    startNode(providedName = DUMMY_NOTARY.name, advertisedServices = setOf(ServiceInfo(SimpleNotaryService.type))),
+                    startNode(providedName = DUMMY_BANK_A.name, rpcUsers = listOf(user)),
+                    startNode(providedName = DUMMY_BANK_B.name)
+            ).map { it.getOrThrow() }
+
+            val nodeARpcAddress = nodeAHandle.configuration.rpcAddress.toString()
+            val nodeARpcClient = RpcClient(nodeARpcAddress)
+
+            // We can ping the notary and Bank B...
+            nodeARpcClient.ping(DUMMY_NOTARY.name.toString())
+            nodeARpcClient.ping(DUMMY_BANK_B.name.toString())
+            // ...but not Bank C, who isn't on the network
+            assertFailsWith<IllegalArgumentException> {
+                nodeARpcClient.ping(DUMMY_BANK_C.name.toString())
+            }
+        }
+    }
+}
---
+++ cordapp/src/main/kotlin/com/template/Client.kt
@@ -0,0 +1,63 @@
+package com.template
+
+import net.corda.client.rpc.CordaRPCClient
+import net.corda.client.rpc.CordaRPCConnection
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.messaging.CordaRPCOps
+import net.corda.core.messaging.startFlow
+import net.corda.core.utilities.NetworkHostAndPort.Companion.parse
+import net.corda.core.utilities.getOrThrow
+import net.corda.core.utilities.loggerFor
+
+val RPC_USERNAME = "user1"
+val RPC_PASSWORD = "test"
+
+fun main(args: Array<String>) {
+    require(args.size == 2) { "Usage: RpcClient <node address> <counterpartyName>" }
+    val rpcAddressString = args[0]
+    val counterpartyName = args[1]
+
+    val rpcClient = RpcClient(rpcAddressString)
+    rpcClient.ping(counterpartyName)
+}
+
+/** An example RPC client that connects to a node and performs various example operations. */
+class RpcClient(rpcAddressString: String) {
+    companion object {
+        val logger = loggerFor<RpcClient>()
+    }
+
+    val rpcConnection: CordaRPCConnection
+
+    init {
+        rpcConnection = establishRpcConnection(rpcAddressString, RPC_USERNAME, RPC_PASSWORD)
+    }
+
+    /** Returns a [CordaRPCConnection] to the node listening on [rpcPortString]. */
+    private fun establishRpcConnection(rpcAddressString: String, username: String, password: String): CordaRPCConnection {
+        val nodeAddress = parse(rpcAddressString)
+        val client = CordaRPCClient(nodeAddress)
+        return client.start(username, password)
+    }
+
+    fun ping(counterpartyName: String) {
+        val rpcProxy = rpcConnection.proxy
+
+        pingCounterparty(rpcProxy, counterpartyName)
+
+        // println prints to IntelliJ console.
+        // TODO: Make logger output show in IntelliJ console.
+        println("Successfully pinged $counterpartyName.")
+        logger.info("Successfully pinged $counterpartyName.")
+    }
+
+    /** Pings the counterparty. */
+    private fun pingCounterparty(rpcProxy: CordaRPCOps, counterpartyName: String) {
+        val counterpartyX500Name = CordaX500Name.parse(counterpartyName)
+        val counterparty = rpcProxy.wellKnownPartyFromX500Name(counterpartyX500Name)
+                ?: throw IllegalArgumentException("Peer $counterpartyName not found in the network map.")
+
+        val flowFuture = rpcProxy.startFlow(::Ping, counterparty).returnValue
+        flowFuture.getOrThrow()
+    }
+}
